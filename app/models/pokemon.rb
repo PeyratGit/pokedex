520e66ab390a4c47d6542ac511dcecbddca9e7ff
clement peyrat <aiglefeu31@gmail.com>
2022-03-17 14:58:23 +0100

    Changed types offense calcul

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -28,13 +28,21 @@ class Pokemon < ApplicationRecord
 
   def weaknesses_double
     $type_double_damage_from.each do |type|
-      $all_types_double_from[:"#{type}"] += 1
+      if $all_types_double_from[:"#{type}"] == 1
+        $all_types_double_from[:"#{type}"] *= 2
+      else
+        $all_types_double_from[:"#{type}"] *= 2
+      end
     end
   end
 
   def weaknesses_half
     $type_half_damage_from.each do |type|
-      $all_types_half_from[:"#{type}"] = 0.5
+      if $all_types_half_from[:"#{type}"] == 1
+        $all_types_half_from[:"#{type}"] = 0.5
+      else
+        $all_types_half_from[:"#{type}"] = 0.25
+      end
     end
   end
 
@@ -77,4 +85,72 @@ class Pokemon < ApplicationRecord
     weaknesses
     return $all_types_defense.select { |type, value| value < 1 }
   end
+
+
+
+
+
+
+  def offensetype1
+    type1 = Type.find_by(name: self.type_1)
+    type1.double_damage_to.each do |type|
+      $type_double_damage_to << type.scan(/name.{1}=>.{1}([a-zA-Z]+)/)[0][0]
+    end
+    type1.no_damage_to.each do |type|
+      $type_no_damage_to << type.scan(/name.{1}=>.{1}([a-zA-Z]+)/)[0][0]
+    end
+  end
+
+  def offensetype2
+    type2 = Type.find_by(name: self.type_2)
+    type2.double_damage_to.each do |type|
+      $type_double_damage_to << type.scan(/name.{1}=>.{1}([a-zA-Z]+)/)[0][0]
+    end
+    type2.no_damage_to.each do |type|
+      $type_no_damage_to << type.scan(/name.{1}=>.{1}([a-zA-Z]+)/)[0][0]
+    end
+  end
+
+  def offense_double
+    $type_double_damage_to.each do |type|
+        $all_types_double_to[:"#{type}"] *= 2
+    end
+  end
+
+  def offense_no
+    $type_no_damage_to.each do |type|
+      $all_types_no_to[:"#{type}"] = 0
+    end
+  end
+
+  def offense
+    $type_double_damage_to = []
+    $type_no_damage_to = []
+    $all_types_offense = { bug: 1, dark: 1, dragon: 1, electric: 1, fairy: 1, fighting: 1, fire: 1, flying: 1, ghost: 1, grass: 1, ground: 1, ice: 1, normal: 1, poison: 1,
+                           psychic: 1, rock: 1, steel: 1, water: 1 }
+    $all_types_double_to = { bug: 1, dark: 1, dragon: 1, electric: 1, fairy: 1, fighting: 1, fire: 1, flying: 1, ghost: 1, grass: 1, ground: 1, ice: 1, normal: 1, poison: 1,
+                           psychic: 1, rock: 1, steel: 1, water: 1 }
+    $all_types_no_to = { bug: 1, dark: 1, dragon: 1, electric: 1, fairy: 1, fighting: 1, fire: 1, flying: 1, ghost: 1, grass: 1, ground: 1, ice: 1, normal: 1, poison: 1,
+                           psychic: 1, rock: 1, steel: 1, water: 1 }
+    offensetype1
+    if self.type_2.nil? == false
+      offensetype2
+    end
+    offense_double
+    offense_no
+    $all_types_offense.each do |key, value|
+      $all_types_offense[:"#{key}"] = $all_types_double_to[:"#{key}"] * $all_types_no_to[:"#{key}"]
+    end
+  end
+
+  def offense_damage_given
+    offense
+    return $all_types_offense.select { |type, value| value > 1 }
+  end
+
+  def resistances_damage_given
+    offense
+    return $all_types_offense.select { |type, value| value < 1 }
+  end
+
 end
