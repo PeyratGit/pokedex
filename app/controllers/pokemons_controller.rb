d5580951840d833869e42c3f041404f16aed9a8c
clement peyrat <aiglefeu31@gmail.com>
2022-03-31 14:40:31 +0200

    Implemented search bar feature. Works with pokemon name. Need style

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,6 +1,14 @@
 class PokemonsController < ApplicationController
   def index
     @pokemons = Pokemon.all
+    if params[:query].present?
+      @pokemons = @pokemons.where('name ILIKE ?', "%#{params[:query]}%")
+    end
+
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: 'list.html', locals: { pokemons: @pokemons } }
+    end
   end
 
   def show
