c65af7f239d0eafb79b438ffd295213e57cc0f33
clement peyrat <aiglefeu31@gmail.com>
2022-04-11 15:54:27 +0200

    Css working. Need style.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,3 +20,11 @@ import { autocompleteSearch } from "../plugins/autocomplete"
 document.addEventListener('turbolinks:load', () => {
   autocompleteSearch();
 })
+
+var checkList = document.getElementById('list1');
+checkList.getElementsByClassName('anchor')[0].onclick = function (evt) {
+  if (checkList.classList.contains('visible'))
+    checkList.classList.remove('visible');
+  else
+    checkList.classList.add('visible');
+}
